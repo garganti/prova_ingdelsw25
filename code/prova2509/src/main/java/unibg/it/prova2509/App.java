ec06c95260f0ff280b8944c0bf55505a168bfff5
Angelo Gargantini <angelo.gargantini@unibg.it>
2025-09-25 16:17:12 +0200

    progetto iniziale

---
+++ code/prova2509/src/main/java/unibg/it/prova2509/App.java
@@ -0,0 +1,10 @@
+package unibg.it.prova2509;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ code/prova2509/src/test/java/unibg/it/prova2509/AppTest.java
@@ -0,0 +1,19 @@
+package unibg.it.prova2509;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
